e87930f9b4da192c93ca59ef3fbcddae0008b9eb
MgKyaw <mgkyaw20112@gmail.com>
2025-01-04 13:01:46 +0700

    feat: Add Hosted Service 'IndexCreationService' to Services

--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
+using Redis.OM.Skeleton.HostedServices;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddHostedService<IndexCreationService>();
 
 var app = builder.Build();
 
