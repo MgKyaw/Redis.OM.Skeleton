dd5614b669b10e4a500dc30d63f62446fd9bc926
MgKyaw <mgkyaw20112@gmail.com>
2025-01-04 15:50:20 +0700

    feat: Map controllers at .net9.0

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using Redis.OM.Skeleton.HostedServices;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
+
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
@@ -14,11 +16,16 @@ builder.Services.AddSingleton(new RedisConnectionProvider(
 
 var app = builder.Build();
 
+app.MapControllers();
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
 
 var summaries = new[]
 {
@@ -27,7 +34,7 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
+    var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
